7469c16e51fd24862ec13e7d6fcb6da2be0cfdbd
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-09-26 21:07:29 -0300

    Ajusta espacamento em diferenciais

--- src/components/diferenciais/diferenciais.component.jsx
+++ src/components/diferenciais/diferenciais.component.jsx
@@ -12,19 +12,19 @@ const Diferenciais = () => {
                 <strong>NOSSOS DIFERENCIAS</strong>
             </h4>
             <Row className="diferenciais__row">
-                <Col className="diferenciais__column">
+                <Col lg={2} md={3} sm={6} xs={12} className="diferenciais__column">
                     <BiLineChartDown size={120} />
                     <span>Menos Gasto</span>
                 </Col>
-                <Col className="diferenciais__column">
+                <Col lg={2} md={3} sm={6} xs={12} className="diferenciais__column">
                     <BiHourglass size={120} />
                     <span>Mais tempo livre</span>
                 </Col>
-                <Col className="diferenciais__column">
+                <Col lg={2} md={3} sm={6} xs={12} className="diferenciais__column">
                     <HiLightningBolt size={120} />
                     <span>Alta produtividade</span>
                 </Col>
-                <Col className="diferenciais__column">
+                <Col lg={2} md={3} sm={6} xs={12} className="diferenciais__column">
                     <GiInjustice size={120} />
                     <span>Redução de horas trabalhistas</span>
                 </Col>
