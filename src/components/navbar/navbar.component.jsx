5398fadb666d0a938f34ff6284056d89a564e964
Bruno Cordeiro <brunocordeiro180@gmail.com>
2021-10-22 18:50:02 -0300

    Identa navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -10,40 +10,39 @@ const Menu = () => {
     const handleShow = () => setShow(true);
 
     return (
-      
-        <div>
 
-        <Modal show={show} onHide={handleClose}>
+        <>
+            <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                     <Modal.Title>GRUPO LBR SERVIÇOS</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>Olá, Prezado/a Candidato/a! 
-                    <p/> Envie seu currículo para contato@grupolbrservicos.com.br 
-                    <p/> Agradecemos o seu interesse em fazer parte do Grupo LBR Serviços.</Modal.Body>
+                <Modal.Body>Olá, Prezado/a Candidato/a!
+                    <p /> Envie seu currículo para contato@grupolbrservicos.com.br
+                    <p /> Agradecemos o seu interesse em fazer parte do Grupo LBR Serviços.</Modal.Body>
                 <Modal.Footer>
                     <Button variant="primary" onClick={handleClose} bg="dark" variant="dark" >
-                    Fechar
+                        Fechar
                     </Button>
                 </Modal.Footer>
-                </Modal>
-      
-      <Navbar className="d-flex mynav" bg="dark" variant="dark" expand="lg">
-            <Container className="myNav__container">
-                <Navbar.Brand className="mynav__link" href="#home">Grupo LBR Serviços</Navbar.Brand>
-                <Navbar.Toggle  />
-                <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-                    <Nav className=" justify-content-end me-auto">
-                        <Nav.Link className="mynav__link" href="#root">HOME</Nav.Link>
-                        <Nav.Link className="mynav__link" href="#servicos">SERVIÇOS</Nav.Link>
-                        <Nav.Link className="mynav__link" href="#diferenciais">DIFERENCIAIS</Nav.Link>
-                        <Nav.Link className="mynav__link" href="#contato">CONTATO</Nav.Link>
-                        <Nav.Link className="mynav__link" onClick={handleShow}>TRABALHE CONOSCO</Nav.Link>
-                        
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </div>
+            </Modal>
+
+            <Navbar className="d-flex mynav" bg="dark" variant="dark" expand="lg">
+                <Container className="myNav__container">
+                    <Navbar.Brand className="mynav__link" href="#home">Grupo LBR Serviços</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+                        <Nav className=" justify-content-end me-auto">
+                            <Nav.Link className="mynav__link" href="#root">HOME</Nav.Link>
+                            <Nav.Link className="mynav__link" href="#servicos">SERVIÇOS</Nav.Link>
+                            <Nav.Link className="mynav__link" href="#diferenciais">DIFERENCIAIS</Nav.Link>
+                            <Nav.Link className="mynav__link" href="#contato">CONTATO</Nav.Link>
+                            <Nav.Link className="mynav__link" onClick={handleShow}>TRABALHE CONOSCO</Nav.Link>
+
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     )
 }
 
